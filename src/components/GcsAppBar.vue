632c31b327c6f96884a66c5ba9d551323758390f
gbsmfather <iyahn@keti.re.kr>
2021-09-12 21:49:23 +0900

    - 이전 접속 정보 저장 (cookies에서 localStorage로 변경)

--- src/components/GcsAppBar.vue
+++ src/components/GcsAppBar.vue
@@ -429,7 +429,8 @@
                 open: false,
                 MOBIUS_DISCONNECTION_TEXT: 'Disconnect',
                 MOBIUS_CONNECTION_TEXT: 'Connect',
-                MOBIUS_CONNECTION_CONNECTED: this.$cookies.isKey('mobius_connected')?(this.$cookies.get('mobius_connected') === 'true'):false,
+                //MOBIUS_CONNECTION_CONNECTED: this.$cookies.isKey('mobius_connected')?(this.$cookies.get('mobius_connected') === 'true'):false,
+                MOBIUS_CONNECTION_CONNECTED: localStorage.getItem('mobius_connected')?(localStorage.getItem('mobius_connected') === 'true'):false,
                 MOBIUS_CONNECTION_DISABLED: false,
                 update_idx: 0,
                 formHasErrors: false,
@@ -479,12 +480,14 @@
                     v => !(v > 254) || '드론 시스템 아이디는 255를 넘을 수 없습니다.',
                     v => !(v <= 0) || '드론 시스템 아이디는 1 이상의 수만 입력 가능합니다.'
                 ],
-                host: this.$cookies.isKey('mobius_host')?(this.$cookies.get('mobius_host')):(this.$store.state.VUE_APP_MOBIUS_HOST),
+                //host: this.$cookies.isKey('mobius_host')?(this.$cookies.get('mobius_host')):(this.$store.state.VUE_APP_MOBIUS_HOST),
+                host: localStorage.getItem('mobius_host')?(localStorage.getItem('mobius_host')):(this.$store.state.VUE_APP_MOBIUS_HOST),
                 host_rule: [
                     v => !!v || '호스트 주소는 필수 입력사항입니다.',
                     v => /^[.0-9]*$/.test(v) || '호스트 주소는 숫자만 입력 가능합니다.'
                 ],
-                gcs: this.$cookies.isKey('mobius_gcs')?(this.$cookies.get('mobius_gcs')):(this.$store.state.VUE_APP_MOBIUS_GCS),
+                //gcs: this.$cookies.isKey('mobius_gcs')?(this.$cookies.get('mobius_gcs')):(this.$store.state.VUE_APP_MOBIUS_GCS),
+                gcs: localStorage.getItem('mobius_gcs')?(localStorage.getItem('mobius_gcs')):(this.$store.state.VUE_APP_MOBIUS_GCS),
                 gcs_rule: [
                     v => !!v || 'GCS 이름은 필수 입력사항입니다.',
                     v => !/[~!@#$%^&*()+|<>?:{}]/.test(v) || 'GCS 이름에는 특수문자를 사용할 수 없습니다.'
@@ -659,8 +662,15 @@
                 this.$store.state.VUE_APP_MOBIUS_HOST = this.host;
                 this.$store.state.VUE_APP_MOBIUS_GCS = this.gcs;
 
-                this.$cookies.set('mobius_host', this.host);
-                this.$cookies.set('mobius_gcs', this.gcs);
+                //this.$cookies.set('mobius_host', this.host);
+                //this.$cookies.set('mobius_gcs', this.gcs);
+
+                //console.log('localStorage-mobius_gcs', localStorage.getItem("mobius_gcs"));
+
+                localStorage.setItem('mobius_host', this.host);
+                localStorage.setItem('mobius_gcs', this.gcs);
+
+
 
                 let self = this;
 
@@ -704,7 +714,8 @@
 
                             self.MOBIUS_CONNECTION_CONNECTED = true;
                             self.$store.state.MOBIUS_CONNECTION_CONNECTED = true;
-                            self.$cookies.set('mobius_connected', self.MOBIUS_CONNECTION_CONNECTED);
+                            //self.$cookies.set('mobius_connected', self.MOBIUS_CONNECTION_CONNECTED);
+                            localStorage.setItem('mobius_connected', self.MOBIUS_CONNECTION_CONNECTED);
                         }
                         else if(res.status === 404) {
                             axios({
@@ -793,7 +804,8 @@
                                                     self.MOBIUS_CONNECTION_CONNECTED = true;
                                                     self.$store.state.MOBIUS_CONNECTION_CONNECTED = true;
 
-                                                    self.$cookies.set('mobius_connected', self.MOBIUS_CONNECTION_CONNECTED);
+                                                    //self.$cookies.set('mobius_connected', self.MOBIUS_CONNECTION_CONNECTED);
+                                                    localStorage.setItem('mobius_connected', self.MOBIUS_CONNECTION_CONNECTED);
                                                 }
                                             ).catch(
                                                 function (err) {
@@ -828,7 +840,8 @@
                 this.MOBIUS_CONNECTION_CONNECTED = false;
                 this.$store.state.MOBIUS_CONNECTION_CONNECTED = false;
 
-                this.$cookies.set('mobius_connected', this.MOBIUS_CONNECTION_CONNECTED);
+                //this.$cookies.set('mobius_connected', this.MOBIUS_CONNECTION_CONNECTED);
+                localStorage.setItem('mobius_connected', this.MOBIUS_CONNECTION_CONNECTED);
 
                 this.confirmSelected();
             },
