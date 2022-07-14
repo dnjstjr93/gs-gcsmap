c6a32fe84f34686356b946925c1313198e77822c
ryeubi <iyahn@keti.re.kr>
2022-07-14 20:26:56 +0900

    - 데이터 로딩 오류 수정 2

--- src/components/GcsAppBar.vue
+++ src/components/GcsAppBar.vue
@@ -1678,6 +1678,9 @@ export default {
                             });
                         }
                     }
+                    else {
+                        callback();
+                    }
                 });
             });
         },
