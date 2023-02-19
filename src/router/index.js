7c817ce1338ab480fe4adc88dc0723da4a6d1add
gbsmfather <iyahn@keti.re.kr>
2023-02-19 10:12:13 +0900

    - 깃 푸시 바로 잡음

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import PureunAir from '../views/PureunAir.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
     {
         path: '/',
-        name: 'PureunAir',
-        component: PureunAir
+        name: 'Home',
+        component: Home
     }
 ]
 
